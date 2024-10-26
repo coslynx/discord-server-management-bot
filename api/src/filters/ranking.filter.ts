4739ad0e405dc9ab3e923c034aa1c584bee52052
CosLynx AI <drixisgod@gmail.com>
2024-10-27 00:46:14 +0530

    generated file: `api/src/filters/ranking.filter.ts`

---
+++ api/src/filters/ranking.filter.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext, SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { RankingEntity } from '../entities/ranking.entity';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+@Injectable()
+export class RankingFilter implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user: RankingEntity = request.user;
+    return !!user;
+  }
+}
